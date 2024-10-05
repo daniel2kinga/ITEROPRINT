dfcb731daf1ff7b65ef01080a83bf07ec1ed2050
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-06 00:33:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -189,5 +189,5 @@ def extraer_pagina():
 
 # Iniciar la aplicación Flask
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
